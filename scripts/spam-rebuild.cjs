18d3f96425bc239dc9413b1cadaecf3e3618cf7d
Sam Magura <srmagura@gmail.com>
2023-04-05 15:55:29 -0400

    spam-rebuild

---
+++ scripts/spam-rebuild.cjs
@@ -0,0 +1,21 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const DELAY = 500;
+
+async function runAsync() {
+  const thePath = path.resolve(__dirname, "../src/main.js");
+
+  for (i = 0; true; i++) {
+    let js = await fs.readFile(thePath, { encoding: "utf-8" });
+
+    js += `// blah`;
+
+    await fs.writeFile(thePath, js, { encoding: "utf-8" });
+    console.log(i);
+
+    await new Promise((resolve) => setTimeout(resolve, DELAY));
+  }
+}
+
+runAsync().catch(console.error);
